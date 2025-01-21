ea42060e0ac3993e7fbbd4394e01bf3df31060a4
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-21 20:43:23 +0530

    added ecommerce website image for portfolio 👌

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/navbar";
+import { FcApproval } from "react-icons/fc";
 
 export default function PortfolioPage(){
     return(
@@ -6,6 +7,24 @@ export default function PortfolioPage(){
             <div className="flex">
             <h2 className="text-4xl font-bold pt-6 text-yellow-400">Portfolio</h2>
             <Navbar/>
+          </div>
+          <div>
+          <div className="bg-gray-800 p-6 rounded-2xl shadow-md">
+            <div className="flex">
+            <FcApproval className="size-7"/>
+              <h2 className="text-white text-xl ml-4">Cosmetics Managment Ecommerce Website</h2>
+            </div>
+            <div className="flex-wrap flex">
+              <img className="w-[300px] overflow-hidden rounded-xl m-4" 
+              src="/cosmeticsSite/cos1.png"/>
+              <img className="w-[300px] overflow-hidden rounded-xl m-4" 
+              src="/cosmeticsSite/cos2.png"/>
+              <img className="w-[300px] overflow-hidden rounded-xl m-4" 
+              src="/cosmeticsSite/cos3.png"/>
+              <img className="w-[300px] overflow-hidden rounded-xl m-4" 
+              src="/cosmeticsSite/cos4.png"/>
+            </div>
+            </div>
           </div>
         </div>
     )
