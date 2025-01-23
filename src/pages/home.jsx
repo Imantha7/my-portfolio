ff8cc857da74d5f4c25c9dda717a09bcdb1761c8
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-23 08:54:27 +0530

    added symbols for my details 🎈

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,42 +9,63 @@ import ContactPage from './contact';
 import { IoLogoLinkedin } from "react-icons/io5";
 import { ImFacebook2 } from "react-icons/im";
 import { IoLogoGithub } from "react-icons/io5";
+import { MdMarkEmailUnread } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoLocation } from "react-icons/io5";
+import { FaBirthdayCake } from "react-icons/fa";
+import { LiaBirthdayCakeSolid } from "react-icons/lia";
+import { FaPhoneVolume } from "react-icons/fa";
 
 export default function HomePage() {
   return (
      <div className="flex w-full h-screen bg-gray-900">
        {/* Left Section */}
-       <div className="w-[30%] flex flex-col items-center justify-center bg-gray-800 py-8">
-         <div>
-         <div className="w-56 h-56 rounded-full overflow-hidden border-4 border-yellow-500 shadow-lg">
+       <div className="w-[30%] flex flex-col justify-center bg-gray-800 py-8">
+         <div className="border-gray-900 border-[10px] rounded-3xl mr-4">
+        <div className='m-6'>
+         <div className="w-56 h-56 rounded-full overflow-hidden 
+         border-4 border-yellow-500 shadow-lg ml-8">
            <img
              src='/my.png' // Path to your image
              alt="Profile"
              className="w-full h-full object-cover"
            />
          </div>
-         <h1 className="mt-6 text-3xl font-bold text-white items-center ml-[40px]">Imantha Piyumal</h1>
-         <p className="text-yellow-400 text-xl mt-2 ml-[40px]">Fullstack Developer</p>
+         <h1 className="mt-6 text-3xl font-bold text-white">Imantha Piyumal</h1>
+         <p className="text-yellow-400 text-xl mt-2 ml-[100px]">Fullstack Developer</p>
          <div className="m-6 ml-10">
-           <div className="m-3">
+           <div className="m-3 flex ">
+              <MdMarkEmailUnread className='bg-yellow-600 size-8 rounded-lg'/>
+              
+              <div className='ml-4'>
                <h1 className="text-gray-400 text-[14px]">EMAIL</h1>
                <span className="text-white">imanthapiyumal7@gmail.com</span>
+               </div>
            </div>
-           <div className="m-3">
+           <div className="m-3 flex ">
+           <FaPhoneVolume className='bg-yellow-600 size-8 rounded-lg'/>
+              <div className='ml-4'>
                <h1 className="text-gray-400 text-[14px]">PHONE</h1>
                <span className="text-white">+94 772943337</span>
+               </div>
            </div>
-           <div className="m-3">
+           <div className="m-3 flex ">
+              <FaBirthdayCake className='bg-yellow-600 size-8 rounded-lg'/>
+              <div className='ml-4'>
                <h1 className="text-gray-400 text-[14px]">BIRTHDAY</h1>
                <span className="text-white">March 7, 1999</span>
+               </div>
            </div>
-           <div className="m-3">
+           <div className="m-3 flex ">
+              <IoLocation className='bg-yellow-600 size-8 rounded-lg'/>
+              <div className='ml-4'>
                <h1 className="text-gray-400 text-[14px]">LOCATION</h1>
                <span className="text-white">Colombo , Sri Lanka</span>
+               </div>
            </div>
 
          </div>
-         <div className='flex m-2'>
+         <div className='flex m-2 items-center'>
             <Link to="https://www.facebook.com/share/15Ct87fBzi/?mibextid=wwXIfr">
             <ImFacebook2 className='size-7 m-3'/></Link>
             <Link to="https://github.com/Imantha7">
@@ -52,6 +73,7 @@ export default function HomePage() {
             <Link to="https://www.linkedin.com/in/imantha-piyumal-9bb3a9253/">
             <IoLogoLinkedin className='size-7 m-3'/></Link>
             
+         </div>
          </div>
          </div>
        </div>
@@ -65,7 +87,6 @@ export default function HomePage() {
            <Route path="/contact" element={<ContactPage/>} />
          </Routes>
        </div>
-      
      </div>
     
   );
